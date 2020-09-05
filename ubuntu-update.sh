417ed0921f976bd5f2e3aafd1166a334a0742b2a
Tom <abaotom14@gmail.com>
2020-09-05 08:16:07 +0800

    add ubuntu update

---
+++ ubuntu-update.sh
@@ -0,0 +1,7 @@
+# https://askubuntu.com/questions/196768/how-to-install-updates-via-command-line
+# Fetches the list of available updates
+sudo apt-get update
+# Strictly upgrades the current packages
+sudo apt-get upgrade
+# Installs updates (new ones)
+sudo apt-get dist-upgrade
